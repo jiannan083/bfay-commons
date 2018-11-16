2e5c6afa3698d8b6e194d1c5a18c97c0a9db1c55
bfay <584633512@qq.com>
2018-11-16 17:38:49 +0800

    1.0.4:添加初始化bean

---
+++ src/main/java/cn/bfay/commons/AutoConfiguration.java
@@ -0,0 +1,42 @@
+package cn.bfay.commons;
+
+import cn.bfay.commons.okhttp.OkHttpUtils;
+import cn.bfay.commons.redis.RedisUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自动化配置.
+ *
+ * @author wangjiannan
+ */
+@Configuration //开启配置
+public class AutoConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(AutoConfiguration.class);
+
+    @Bean
+    @ConditionalOnMissingBean//缺失时，初始化bean并添加到SpringIoc
+    public RedisUtils redisUtils() {
+        log.info(">>>The RedisUtils Not Found，Execute Create New Bean.");
+        return new RedisUtils();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean//缺失时，初始化bean并添加到SpringIoc
+    public OkHttpUtils okHttpUtils() {
+        log.info(">>>The OkHttpUtils Not Found，Execute Create New Bean.");
+        return new OkHttpUtils();
+    }
+
+    //@Bean
+    //@ConditionalOnMissingBean//缺失时，初始化bean并添加到SpringIoc
+    //public RedisTime redisTime() {
+    //    log.info(">>>The RedisTime Not Found，Execute Create New Bean.");
+    //    return new RedisTime();
+    //}
+
+}
+
